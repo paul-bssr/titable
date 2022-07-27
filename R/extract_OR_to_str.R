ca01e46258042583a175f7e4025cae6c90eb0a5b
paul-bssr <paul_brasseur@hotmail.fr>
2022-07-27 17:35:48 +0200

    Adding documentation for extract_OR_to_str function

--- R/extract_OR_to_str.R
+++ R/extract_OR_to_str.R
@@ -1,3 +1,21 @@
+#' Title
+#'
+#' @title Compute and summarize in a string OR, IC, pvalue for a glm model
+#'
+#' @description Compute values for OR, IC, pvalue for a glm model and summarizes
+#' result in a string of the form "OR (IC_min-IC_max, p=pvalue)"
+#'
+#' @param model A glm model corresponding to a logistic regression
+#' (family=binomial)
+#' @param studied_var Name of the variable to explain
+#'
+#' @return A string of the form "OR (IC_min-IC_max, p=pvalue)
+#' @export
+#'
+#' @examples
+#' model_1 <- glm( death_J28 ~ ., data=data, family="binomial")
+#' extract_OR_to_str(model_1, col)
+
 extract_OR_to_str <- function(model, studied_var){
   coef <- extract_OR_from_model(model, studied_var)
   str_final <- str_transform(
