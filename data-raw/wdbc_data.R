f6b48a03acc292d45dc45b11bf590bad79bead37
paul-bssr <paul_brasseur@hotmail.fr>
2022-08-10 12:44:30 +0200

    Adding data for breast cancer in the package

---
+++ data-raw/wdbc_data.R
@@ -0,0 +1,31 @@
+## code to prepare `wdbc_data` dataset goes here
+
+# retrieve paths to datafiles
+data.file <- system.file(
+  "extdata",
+  "wdbc.data",
+  package = "titable"
+)
+
+
+# read the two .csv files
+wdbc.data <- read.csv(
+  data.file,
+  stringsAsFactors = TRUE,
+  encoding = "UTF-8",
+  header = FALSE
+)
+
+
+# Column selection (12 first)
+wdbc.data <- wdbc.data[,1:12]
+
+# column renaming
+colnames(wdbc.data) <- c("person_id", "diagnosis", "radius", "texture",
+                         "perimeter", "area", "smoothness", "compactness",
+                         "concavity", "concave_points", "symetry",
+                         "fractal_dimension"
+                         )
+
+
+usethis::use_data(wdbc.data, overwrite = TRUE)
