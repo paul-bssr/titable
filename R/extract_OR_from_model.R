dba6a14724ec26da841b92ef92234e869c622bed
paul-bssr <paul_brasseur@hotmail.fr>
2022-09-07 10:07:17 +0200

    modification of extract_OR_from_model to take into account categorical data

--- R/extract_OR_from_model.R
+++ R/extract_OR_from_model.R
@@ -20,24 +20,37 @@
 #'                 family="binomial")
 #' extract_OR_from_model(model_1, studied_var = "texture")
 #'
-extract_OR_from_model <- function(model, studied_var){
+extract_OR_from_model <- function(model, studied_var, level=NULL){
 
   # Checking type of inputs
   stopifnot("Model must be binomial family glm" =
             (model$family$family == "binomial") )
   stopifnot("Input studied_var must be a character" =
             ( class(studied_var) == "character" ) )
+  stopifnot("studied_var is factor, thus a level name should be indicated" =
+              !(
+                is.factor( wdbc.data[[studied_var]] )
+                & is.null(level)
+                )
+            )
+
+  # Variable to look for
+  if ( !is.null(level) ){
+    coef_var <- paste(studied_var, level, sep="")
+  } else {
+    coef_var <- studied_var
+  }
 
   # OR, p extraction from coef
   coef = coef(summary(model))
-  cond_studied_var_coef = grepl( studied_var , rownames(coef))
-  OR = exp( coef[cond_studied_var_coef ,"Estimate"] )
-  p = coef[cond_studied_var_coef,"Pr(>|z|)"]
+  index_var = which(rownames(coef)==coef_var)
+  OR = exp( coef[index_var ,"Estimate"] )
+  p = coef[index_var,"Pr(>|z|)"]
 
   # IC_min, IC_max extraction from confint
   conf_int = confint(model)
-  cond_conf_int = grepl( studied_var , rownames(conf_int))
-  IC_min = exp( conf_int[cond_conf_int, "2.5 %"] )
-  IC_max = exp( conf_int[cond_conf_int, "97.5 %"] )
+  index_conf_int = which(rownames(conf_int)==coef_var)
+  IC_min = exp( conf_int[index_conf_int, "2.5 %"] )
+  IC_max = exp( conf_int[index_conf_int, "97.5 %"] )
   return( c(OR, IC_min, IC_max, p) )
 }
