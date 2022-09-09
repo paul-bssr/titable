5bc7ad49ad5a1b7a0ed077fc3dbfef0b61b9b7dd
paul-bssr <paul_brasseur@hotmail.fr>
2022-09-09 16:11:51 +0200

    Adding unit test for multivariate OR with categorical variable

--- tests/testthat/test-summary_table.R
+++ tests/testthat/test-summary_table.R
@@ -193,13 +193,82 @@ test_that("Checking OR multivariate computation for quantitative variables", {
   )
 
   expect_equal(
-    table[ table$label=="radius", "OR (model1)"],
+    table[ table$label=="radius", "OR (model 1)"],
     OR_radius_str
   )
 
   expect_equal(
-    table[ table$label=="radius", "OR (model2)"],
+    table[ table$label=="radius", "OR (model 2)"],
     OR_radius_str_2
+  )
+
+})
+
+
+
+test_that("Checking OR miultiivariate computation for qualitative variables", {
+
+  table <- summary_table(data = wdbc.data,
+                         studied_vars = c("radius", "texture",
+                                          "compactness_quartile"),
+                         dependent = "diagnosis",
+                         univariate = TRUE,
+                         multivariate = list(c("smoothness"),
+                                             c("radius", "compactness_quartile",
+                                               "compactness_binary"))
+  )
+
+
+  ### Testing compactness_quartile (qualitative variable)
+  model_1 <- glm( diagnosis ~ compactness_quartile + smoothness ,
+                data = wdbc.data, family=binomial )
+
+  level_sel <- levels(wdbc.data$compactness_quartile)[3]
+  name_var <- paste("compactness_quartile", level_sel, sep="")
+
+  OR_cq <- round( exp( coef(summary(model_1)) )[name_var, "Estimate"],3)
+  IC_min_cq <- round( exp(confint(model_1)[name_var,"2.5 %"]), 3)
+  IC_max_cq <- round( exp(confint(model_1)[name_var,"97.5 %"]), 3)
+  p_value <- round( coef(summary(model_1))[name_var,"Pr(>|z|)"], 3)
+
+
+  OR_cq_str <- paste( round( OR_cq , 3 ) , " (",
+                      round( IC_min_cq, 3 ), "-",
+                      round( IC_max_cq, 3 ), ", p=",
+                      round( p_value, 3 ), ")",
+                      sep=""
+  )
+
+
+  ### Testing compactness_quartile (multi model 2)
+  model_2 <- glm( diagnosis ~ compactness_quartile + radius + compactness_binary,
+                  data = wdbc.data, family=binomial )
+
+  level_sel <- levels(wdbc.data$compactness_quartile)[3]
+  name_var <- paste("compactness_quartile", level_sel, sep="")
+
+  OR_cq_2 <- round( exp( coef(summary(model_2)) )[name_var, "Estimate"],3)
+  IC_min_cq_2 <- round( exp(confint(model_2)[name_var,"2.5 %"]), 3)
+  IC_max_cq_2 <- round( exp(confint(model_2)[name_var,"97.5 %"]), 3)
+  p_value_2 <- round( coef(summary(model_2))[name_var,"Pr(>|z|)"], 3)
+
+
+  OR_cq_str_2 <- paste( round( OR_cq_2 , 3 ) , " (",
+                      round( IC_min_cq_2, 3 ), "-",
+                      round( IC_max_cq_2, 3 ), ", p=",
+                      round( p_value_2, 3 ), ")",
+                      sep=""
+  )
+
+
+  expect_equal(
+    table[ table$levels==level_sel, "OR (model 1)"],
+    OR_cq_str
+  )
+
+  expect_equal(
+    table[ table$levels==level_sel, "OR (model 2)"],
+    OR_cq_str_2
   )
 
 })
